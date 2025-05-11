eb1a0d308006cfb2fa1fb933eb7675aac665cffd
felixdae <felixdae@gmail.com>
2025-05-11 15:50:58 +0800

    lab3

--- lab3/common.py
+++ lab3/common.py
@@ -583,7 +583,7 @@ class CFGTrainer(Trainer):
 
         # Step 3: Sample t and x
         # pass
-        t = torch.rand(batch_size)
+        t = torch.rand(batch_size, 1, 1, 1)
         x = self.path.sample_conditional_path(z, t)
 
         # Step 4: Regress and output loss
